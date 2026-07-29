80b372a2369c12cff2939af80cf2fd48f87e8202
Vansh <vanshparate@gmail.com>
2026-07-29 07:12:15 +0530

    updated tsconfig structure

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,18 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "node",
-    },
+
+        include: [
+            "tests/unit/**/*.test.ts",
+            "tests/integration/**/*.test.ts"
+        ],
+
+        coverage: {
+            provider: "v8",
+            reporter: [
+                "text",
+                "html"
+            ]
+        }
+    }
 });
